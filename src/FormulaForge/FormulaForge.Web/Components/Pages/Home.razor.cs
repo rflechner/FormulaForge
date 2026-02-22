82701d3cc1ca04ac6b5c596191d85b7e02299230
rflechner <romain.flechner@proton.me>
2026-02-22 13:27:06 +0100

    feat: create `Home` page with script editor, inputs/outputs grids, and computation logic

---
+++ src/FormulaForge/FormulaForge.Web/Components/Pages/Home.razor.cs
@@ -0,0 +1,135 @@
+using BlazorMonaco.Editor;
+using FormulaForge.Engine.DomainSpecificLanguage.Ast;
+using FormulaForge.Engine.Runtime;
+using FormulaForge.Engine.Time;
+using FormulaForge.Web.Components.Shared;
+using FormulaForge.Web.Contexts;
+
+namespace FormulaForge.Web.Components.Pages;
+
+public partial class Home
+{
+    private List<DateTime> _months = new();
+    private readonly List<ExcelGrid.RowData> _inputsRows = new();
+    private readonly List<ExcelGrid.RowData> _outputsRows = new();
+    private readonly DynamicDslContext _dslContext = new(new DynamicDataContext());
+    private ScriptInterpreter? _interpreter;
+    private StandaloneCodeEditor? _editor;
+    public CodeRunResult? ScriptRunResult { get; set; }
+
+    private static StandaloneEditorConstructionOptions EditorConstructionOptions(StandaloneCodeEditor editor)
+    {
+        return new StandaloneEditorConstructionOptions
+        {
+            Language = "bash",
+            AutomaticLayout = true,
+            Value = string.Empty
+        };
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        var period = new Period(new DateTime(2024, 1, 1), new DateTime(2027, 1, 1));
+        var months = period.GetMonths().ToArray();
+        _months = months.Select(m => m.InclusiveStart.DateTime).ToList();
+
+        _dslContext.GlobalScope.Variables.Add("x",
+            new RuntimeVariableValue.RuntimeScalarValue(new ScalarValueNode.IntegerScalarValue(10)));
+
+        var random = new Random();
+        var balance = new TimeSeries<ScalarValueNode>();
+        var changeRate = new TimeSeries<ScalarValueNode>();
+
+        for (var index = 0; index < months.Length; index++)
+        {
+            var month = months[index];
+            balance.Add(month, new ScalarValueNode.IntegerScalarValue(index * 100), (a, b) => a.Value);
+            changeRate.Add(month, new ScalarValueNode.DecimalScalarValue((decimal)random.NextDouble()),
+                (a, b) => a.Value);
+        }
+
+        _dslContext.GlobalScope.Variables.Add("balance", new RuntimeVariableValue.RuntimeTimeSeriesValue(balance));
+        _dslContext.GlobalScope.Variables.Add("change_rate",
+            new RuntimeVariableValue.RuntimeTimeSeriesValue(changeRate));
+
+        await LoadRows();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!firstRender) return;
+
+        await LoadRows();
+    }
+
+    private async Task LoadRows()
+    {
+        _inputsRows.Clear();
+        _outputsRows.Clear();
+
+        foreach (var (variableName, variableValue) in _dslContext.GlobalScope.Variables)
+        {
+            _outputsRows.Add(new ExcelGrid.RowData
+            {
+                Name = variableName,
+                Values = Map(variableValue)
+            });
+        }
+
+        StateHasChanged();
+    }
+
+    private async Task RunScript()
+    {
+        if (_editor == null)
+        {
+            StateHasChanged();
+            return;
+        }
+
+        try
+        {
+            _interpreter = new ScriptInterpreter(_dslContext);
+
+            var code = await _editor.GetValue();
+            ScriptRunResult = _interpreter.Run(code);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        await LoadRows();
+    }
+
+    private Dictionary<int, decimal> Map(RuntimeVariableValue variableValue)
+    {
+        switch (variableValue)
+        {
+            case RuntimeVariableValue.RuntimeScalarValue runtimeScalarValue:
+                var value = Map(runtimeScalarValue.Value);
+                return new Dictionary<int, decimal> { { 0, value } };
+
+            case RuntimeVariableValue.RuntimeTimeSeriesValue runtimeTimeSeriesValue:
+                return runtimeTimeSeriesValue.Value
+                    .Select((v, i) => new
+                    {
+                        Value = Map(v.Value),
+                        Index = i
+                    })
+                    .ToDictionary(v => v.Index, v => v.Value);
+
+            default:
+                throw new ArgumentOutOfRangeException(nameof(variableValue));
+        }
+    }
+
+    private static decimal Map(ScalarValueNode scalarValue) =>
+        scalarValue switch
+        {
+            ScalarValueNode.BooleanScalarValue booleanScalarValue => booleanScalarValue.Value ? 1 : 0,
+            ScalarValueNode.DecimalScalarValue decimalScalarValue => decimalScalarValue.Value,
+            ScalarValueNode.IntegerScalarValue integerScalarValue => integerScalarValue.Value,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+}
